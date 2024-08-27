5718ecc2924772b383813d9babffe76e7e6e38f3
unknown <lucas.matheus@ezoom.com.br>
2024-08-27 16:02:40 -0300

    style: estilizando o statusbar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { ActivityIndicator, Text, View } from 'react-native';
+import { ActivityIndicator, StatusBar, Text, View } from 'react-native';
 
 export default function App() {
   const [isFontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold})
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{ flex: 1, backgroundColor: '#202024', alignItems: 'center', justifyContent: 'center'}}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
       {
         isFontLoaded ?
         <Text>Open up App.tsx to start working on your app!</Text>
