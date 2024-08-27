a2d033752f9fcda69db72963786a6968df6fddb6
unknown <lucas.matheus@ezoom.com.br>
2024-08-27 17:18:59 -0300

    chore: configurando o gluestack

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { ActivityIndicator, StatusBar, Text, View } from 'react-native';
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
+import { ActivityIndicator, StatusBar, View } from 'react-native';
 
 export default function App() {
   const [isFontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold})
   return (
-    <View style={{ flex: 1, backgroundColor: '#202024', alignItems: 'center', justifyContent: 'center'}}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {
-        isFontLoaded ?
-        <Text>Open up App.tsx to start working on your app!</Text>
-        :
-        <ActivityIndicator/>
-      }
-      
-    </View>
+    <GluestackUIProvider>
+      <View style={{ flex: 1, backgroundColor: '#202024', alignItems: 'center', justifyContent: 'center'}}>
+        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+        {
+          isFontLoaded ?
+          <Text color='white'>Home</Text>
+          :
+          <ActivityIndicator/>
+        }
+        
+      </View>
+    </GluestackUIProvider>
   );
 }
 
